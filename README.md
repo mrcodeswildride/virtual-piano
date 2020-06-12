67923a4c32531e9cf2d21773dca3894ea23f4361
David Keller <mrcodeswildride@gmail.com>
2020-06-12 21:29:48 +0000

    es6 update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # virtual-piano
 
-This is a virtual piano web app written in HTML, CSS, and JavaScript.
+Virtual piano web app written in HTML, CSS, and JavaScript.
 
 Use this app at [virtualpiano.mrcodeswildride.com](https://virtualpiano.mrcodeswildride.com/).
 Learn how to make this app at [mrcodeswildride.com](https://www.mrcodeswildride.com/).
--- script.js
+++ script.js
@@ -1,95 +1,56 @@
-var keys = document.querySelectorAll(".whiteKey, .blackKey");
-var notesDisplay = document.getElementById("notes");
-var playNotesButton = document.getElementById("playNotes");
-var twinkleButton = document.getElementById("twinkle");
+let keys = document.querySelectorAll(`.white, .black`)
+let notesParagraph = document.getElementById(`notesParagraph`)
+let playButton = document.getElementById(`playButton`)
+let twinkleButton = document.getElementById(`twinkleButton`)
 
-for (var i = 0; i < keys.length; i++) {
-    keys[i].addEventListener("click", pressKey);
+let twinkleNotes = `ccggaag-ffeeddc`
+let keyIds = []
+
+for (let key of keys) {
+  key.addEventListener(`click`, pressKey)
 }
 
-playNotesButton.addEventListener("click", playNotes);
-twinkleButton.addEventListener("click", twinkle);
+playButton.addEventListener(`click`, play)
+twinkleButton.addEventListener(`click`, twinkle)
 
 function pressKey() {
-    var id = this.getAttribute("id");
-    var note = document.getElementById(id + "Note");
-    var noteOutput = id.replace("Sharp", "#");
+  playNote(this.id)
+}
 
-    this.classList.add("selected");
-    note.currentTime = 0;
-    note.play();
+function playNote(keyId) {
+  let key = document.getElementById(keyId)
 
-    if (notesDisplay.innerHTML == "") {
-        notesDisplay.innerHTML = noteOutput;
-    }
-    else {
-        notesDisplay.innerHTML += " " + noteOutput;
-    }
+  if (key != null) {
+    let note = document.getElementById(`${keyId}Note`)
+    note.currentTime = 0
+    note.play()
+  }
 
-    setTimeout(clearSelected, 250, this);
+  keyIds.push(keyId)
+  notesParagraph.innerHTML = keyIds.join(` `).replace(/Sharp/g, `#`)
+
+  if (key != null) {
+    key.classList.add(`selected`)
+    setTimeout(clearSelected, 250, key)
+  }
 }
 
 function clearSelected(key) {
-    key.classList.remove("selected");
+  key.classList.remove(`selected`)
 }
 
-function playNotes() {
-    var notes = notesDisplay.innerHTML.split(" ");
-    notesDisplay.innerHTML = "";
+function play() {
+  let keyIdsToPlay = [...keyIds]
 
-    for (var i = 0; i < notes.length; i++) {
-        setTimeout(playNote, i * 300, notes[i]);
-    }
-}
+  keyIds = []
+  notesParagraph.innerHTML = ``
 
-function playNote(note) {
-    if (note == "c") {
-        keys[0].click();
-    }
-    else if (note == "c#") {
-        keys[1].click();
-    }
-    else if (note == "d") {
-        keys[2].click();
-    }
-    else if (note == "d#") {
-        keys[3].click();
-    }
-    else if (note == "e") {
-        keys[4].click();
-    }
-    else if (note == "f") {
-        keys[5].click();
-    }
-    else if (note == "f#") {
-        keys[6].click();
-    }
-    else if (note == "g") {
-        keys[7].click();
-    }
-    else if (note == "g#") {
-        keys[8].click();
-    }
-    else if (note == "a") {
-        keys[9].click();
-    }
-    else if (note == "a#") {
-        keys[10].click();
-    }
-    else if (note == "b") {
-        keys[11].click();
-    }
-    else if (note == "-") {
-        if (notesDisplay.innerHTML == "") {
-            notesDisplay.innerHTML = "-";
-        }
-        else {
-            notesDisplay.innerHTML += " -";
-        }
-    }
+  for (let i = 0; i < keyIdsToPlay.length; i++) {
+    setTimeout(playNote, i * 300, keyIdsToPlay[i])
+  }
 }
 
 function twinkle() {
-    notesDisplay.innerHTML = "c c g g a a g - f f e e d d c";
-    playNotes();
+  keyIds = twinkleNotes.split(``)
+  play()
 }
